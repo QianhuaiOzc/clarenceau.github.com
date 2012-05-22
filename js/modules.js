1416b9103fff3167e5c00dd583034ed7be48dacc
ClarenceAu <ozhencong@gmail.com>
2012-05-22 13:05:17 +0800

    add sandbox model and package the modules' code

---
+++ js/modules.js
@@ -0,0 +1,30 @@
+Core.registerModule("info", function(sandbox) {
+	var container = null;
+	return {
+		init: function() {
+			container = sandbox.container;
+			sandbox.show(container);
+
+            var str = '{\n"name":"ClarenceAu",\n"sex":"male",\n"email":"ozhencong@gmail.com"\n}'
+	        var infoAsync = eval(Jscex.compile("async", function(element, str) {
+		        for(var i = 0; i < str.length; i++) {
+		            var span = document.createElement("span");
+		            span.innerText = str[i];
+		            container.appendChild(span);
+		            // element.innerHTML += str[i];
+			        $await(Jscex.Async.sleep(100));
+			    }
+
+			    container.innerText = str;
+		    }));
+
+		    infoAsync(container, str).start();
+		},
+
+		destroy: function() {}
+	};
+});
+
+
+
+Core.startAll();
